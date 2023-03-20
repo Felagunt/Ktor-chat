4ccc67aaa7e482b595968f59d9c959ea70b9c706
Ilya <felagunt@live.ru>
2023-03-20 09:34:51 +0300

    setup

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,9 +5,6 @@ import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
+
+
 }
