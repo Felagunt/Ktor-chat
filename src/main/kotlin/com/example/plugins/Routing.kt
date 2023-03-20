9af0e9bf05f04b3c5befc9b7febc4069c9a84df2
Ilya <felagunt@live.ru>
2023-03-20 19:14:00 +0300

    koin di

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,10 +1,17 @@
 package com.example.plugins
 
+import com.example.room.RoomController
+import com.example.room.getAllMessages
+import com.example.routes.chatSocket
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
-
-
+    val roomController by inject<RoomController>()
+    install(Routing) {
+        chatSocket(roomController)
+        getAllMessages(roomController)
+    }
 }
